1643ec45972030a72a56b0a38f8c02379ff7dbf0
WenqiangLiao <18840965351@163.com>
2020-08-03 13:19:27 +0800

    [Wenqiang Liao] feat: add class AFrame

---
+++ src/main/java/AFrame.java
@@ -0,0 +1,9 @@
+public class AFrame {
+    int firstThrow;
+    int secondThrow;
+
+    public AFrame(int firstThrow, int secondThrow) {
+        this.firstThrow = firstThrow;
+        this.secondThrow = secondThrow;
+    }
+}
--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,5 +1,7 @@
 public class BowlingGame {
 
+
+
     public int didNotHitDownAllAfterTwoThrows(int firstThrow, int secondThrow) {
         return firstThrow + secondThrow;
     }
