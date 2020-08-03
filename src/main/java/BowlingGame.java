494cc4fd6ad0b0609c50b05c293dc1c2796b6e3a
WenqiangLiao <18840965351@163.com>
2020-08-03 12:27:17 +0800

    [Wenqiang Liao] feat: should return number of pin when did not hit down all after two throws

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,6 @@
+public class BowlingGame {
+
+    public int didNotHitDownAllAfterTwoThrows(int firstThrow, int secondThrow) {
+        return firstThrow + secondThrow;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingGameTest {
+
+    @Test
+    void should_return_number_of_pin_when_did_not_hit_down_all_after_two_throws () {
+        // Given
+        BowlingGame bowlingGame = new BowlingGame();
+        // When
+        int scoreOfOneFrame = bowlingGame.didNotHitDownAllAfterTwoThrows(2, 3);
+        // Then
+        assertEquals(5, scoreOfOneFrame);
+    }
 }
